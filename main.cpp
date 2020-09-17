4ad28d5ed74b68bf26b8f46a3f73018bd741bc60
Casey Sanchez <nabbz0rz@gmail.com>
2020-09-16 21:49:16 -0400

    commit on 'master'

--- main.cpp
+++ main.cpp
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
         std::shared_ptr<Node> psi = expression_parser.Parse();
 
         for (double radius = 0.0; radius < 2.0; radius += 0.5) {
-            for (double theta = 0.0; theta < 2.0 * 3.14159; theta += 0.25 * 3.14159) {
-                for (double phi = 0.0; phi < 2.0 * 3.14159; phi += 0.25 * 3.14159) {
+            for (double theta = 0.0; theta < 3.14159; theta += 0.5 * 3.14159) {
+                for (double phi = 0.0; phi < 2.0 * 3.14159; phi += 0.5 * 3.14159) {
                     *x = radius * std::sin(theta) * std::cos(phi);
                     *y = radius * std::sin(theta) * std::sin(phi);
                     *z = radius * std::cos(theta);
@@ -34,6 +34,11 @@ int main(int argc, char *argv[])
                 }
             }
         }
+
+        ExpressionComposer expression_composer(psi, node_map);
+
+        std::cout << "Re-composed expression from expression tree: " << std::endl;
+        std::cout << expression_composer.Compose() << std::endl;
     }
     catch(std::exception const &exception) {
         std::cout << exception.what() << std::endl;
