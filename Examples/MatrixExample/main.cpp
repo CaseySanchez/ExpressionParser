2b531268e471fcc606fd44fb520a983a417798e4
Casey Sanchez <nabbz0rz@gmail.com>
2020-11-15 07:01:12 -0500

    commit on 'master'

--- Examples/MatrixExample/main.cpp
+++ Examples/MatrixExample/main.cpp
@@ -1,6 +1,4 @@
-// Evaluates 3D wavefunction at various spherical coordinates where 
-// psi(x, y, z) = pi^{-3/2} * e^{-(x^2+y^2+z^2)}
-// such that int_{-infty}^{infty} int_{-infty}^{infty} int_{-infty}^{infty} psi(x, y, z) dz dy dx = 1
+// Transforms a 2D vector by a 2D rotation matrix
 
 #include <iostream> 
 
@@ -11,21 +9,24 @@
 int main(int argc, char *argv[])
 {
     try {
-        std::shared_ptr<Constant> theta(new Constant(3.14159 / 4.0));
+        std::shared_ptr<Variable> theta(new Variable(3.14159 / 4.0));
         std::shared_ptr<Constant> x(new Constant(1.0));
         std::shared_ptr<Constant> y(new Constant(3.0));
 
         std::map<std::string, std::shared_ptr<Node>> node_map = { { "theta", theta }, { "x", x }, { "y", y } };
 
-        //std::string expression_str = "\\begin{bmatrix} cos\\left(theta\\right) & -sin\\left(theta\\right) \\\\ sin\\left(theta\\right) & cos\\left(theta\\right) \\end{bmatrix} * \\begin{bmatrix} x \\\\ y \\end{bmatrix}";
-        std::string expression_str = "\\begin{bmatrix} \\begin{bmatrix} 1 & 2 \\end{bmatrix} & \\begin{bmatrix} 3 & 4 \\end{bmatrix} \\\\ \\begin{bmatrix} 5 & 6 \\end{bmatrix} & \\begin{bmatrix} 7 & 8 \\end{bmatrix} \\end{bmatrix} * \\begin{bmatrix} \\begin{bmatrix} 1 \\\\ 2 \\end{bmatrix} & \\begin{bmatrix} 3 \\\\ 4 \\end{bmatrix} \\\\ \\begin{bmatrix} 5 \\\\ 6 \\end{bmatrix} & \\begin{bmatrix} 7 \\\\ 8 \\end{bmatrix} \\end{bmatrix}";
-
+        std::string expression_str = "\\begin{bmatrix} cos\\left(theta\\right) & -sin\\left(theta\\right) \\\\ sin\\left(theta\\right) & cos\\left(theta\\right) \\end{bmatrix} * \\begin{bmatrix} x \\\\ y \\end{bmatrix}";
+        
         ExpressionParser expression_parser(expression_str, node_map);
 
         std::shared_ptr<Node> matrix_node(expression_parser.Parse());
 
-        std::cout << expression_str << std::endl << " = " << std::endl << matrix_node << std::endl;
-        
+        for (int i = 1; i <= 8; ++i) {
+            *theta = 2 * 3.14159 * (static_cast<double>(i) / 8.0);
+
+            std::cout << "r(" << static_cast<Node>(*theta) << ") * v = " << matrix_node << std::endl;
+        }
+
         ExpressionComposer expression_composer(matrix_node, node_map);
 
         std::cout << "Re-composed expression from expression tree: " << std::endl;
